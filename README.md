70e9a27b2b204fc8a7fe7b5fb16ee6c1c127255a
Sébastien Grosjean <public@zencocoon.com>
2011-10-16 17:55:06 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# OAuth 2 Client Test Application
+
+A sample OAuth 2 Client application using Rails 3.1 and OmniAuth 1.0
+
+# Live Applications
+
+OAuth 2 Provider Test App: https://testoauth2provider.herokuapp.com
+OAuth 2 Client Test App: https://testoauth2client.herokuapp.com
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestOAuth2Client::Application.config.secret_token = '3f5a224de1e11bfd43ace43b1e0c3f5f44022baf14dfb133825243273aca3fe6538454da2ec73afff3fe40adeccf0e7dd13d88b19d8aa33f7aa8a189aec8e023'
