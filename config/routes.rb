1f57696d4a1a2a9f9ba672e741193c36c6962951
Sébastien Grosjean <public@zencocoon.com>
2011-10-16 18:16:39 +0300

    Add root url

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 TestOAuth2Client::Application.routes.draw do
-  get "home/index"
-
   match "/auth/:provider/callback" => "sessions#create"
   match "/signout" => "sessions#destroy", :as => :signout
+
+  root :to => 'home#index'
 end
