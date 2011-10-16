ef2f2ec7f391c5e51630899892280cd0d54e12cf
Sébastien Grosjean <public@zencocoon.com>
2011-10-16 18:13:48 +0300

    Add landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 TestOAuth2Client::Application.routes.draw do
+  get "home/index"
+
   match "/auth/:provider/callback" => "sessions#create"
   match "/signout" => "sessions#destroy", :as => :signout
 end
