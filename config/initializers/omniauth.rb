6dce8cd665cb73f80f308e310df37db1d5fc2d5e
Sébastien Grosjean <public@zencocoon.com>
2012-05-01 13:14:30 +0300

    Update keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :testoauth2strategy, '27be6f10e2a4b637cdf919d5515d89a6', '4cadb2c97c8a935b51551e70380bfa75'
+  provider :testoauth2strategy, 'b4e45a9b3e44aaf136a4670a700ecdbd', '69f52b9e3b1f9fa7b97597c1b732b51d'
 
   # To use in local, update with your "identifier" and "secret"
   # provider :testoauth2strategy, 'fb2ef6ab13becf3198b73ec37e346941', '78eaff9e05c97be7edcbb09698355d09'
