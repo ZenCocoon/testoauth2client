6ee0516dec20bc3fdde15fd6dade0024f9595e76
goofrider <goofrider@gmail.com>
2013-02-18 22:18:08 +0800

    Instructions on {:provider_ignores_state => true}

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,4 +3,12 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 
   # To use in local, update with your "identifier" and "secret"
   # provider :testoauth2strategy, 'fb2ef6ab13becf3198b73ec37e346941', '78eaff9e05c97be7edcbb09698355d09'
+
+  # if you receive a non-descriptive CallbackError or invalid_credentials when you're certain that the server 
+  # has approved the request, you might be affected by the new "state" check requirement in omniauth-oauth2,
+  # add {:provider_ignores_state => true} in your config like so: (and understand that you're bypassing CSRF check
+  #
+  #  provider :testoauth2strategy, 'fb2ef6ab13becf3198b73ec37e346941', 
+  # '78eaff9e05c97be7edcbb09698355d09', {:provider_ignores_state => true}
+
 end
